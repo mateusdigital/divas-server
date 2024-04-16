4b1574f251297ed5d66b65a31f61bdb550fd3e04
mateusdigital <hello@mateus.digital>
2024-04-16 19:03:11 -0300

    Add model for Item

---
+++ models/Item.js
@@ -0,0 +1,37 @@
+//----------------------------------------------------------------------------//
+//                               *       +                                    //
+//                         '                  |                               //
+//                     ()    .-.,="``"=.    - o -                             //
+//                           '=/_       \     |                               //
+//                        *   |  '=._    |                                    //
+//                             \     `=./`,        '                          //
+//                          .   '=.__.=' `='      *                           //
+//                 +                         +                                //
+//                      O      *        '       .                             //
+//                                                                            //
+//  File      : Item.js                                                       //
+//  Project   : divas-server                                                  //
+//  Date      : 2024-04-16                                                    //
+//  License   : See project's COPYING.TXT for full info.                      //
+//  Author    : mateus.digital <hello@mateus.digital>                         //
+//  Copyright : mateus.digital - 2024                                         //
+//                                                                            //
+//  Description :                                                             //
+//                                                                            //
+//----------------------------------------------------------------------------//
+
+// -----------------------------------------------------------------------------
+const mongoose = require("mongoose");
+
+// -----------------------------------------------------------------------------
+const ItemSchema = new mongoose.Schema({
+  // Info
+  category:  { type: String, required: true },
+  color:     { type: String, required: true },
+});
+
+// -----------------------------------------------------------------------------
+const Item = mongoose.model("Item", ItemSchema);
+
+// -----------------------------------------------------------------------------
+module.exports = Item;
